a91131933cac5dea85c2158ba326d4ba44871e9d
Albie van der Merwe <albievan@gmail.com>
2023-04-10 09:11:33 +0100

    Adding new utility dns-updater for godaddy

---
+++ src/dns-updater/godaddy/README.md
@@ -0,0 +1,66 @@
+# About
+This utility provides the capability to dynamically update GoDaddy DNS records and seamlessly add any missing records with your ip address your isp allocates you.
+
+The Dockerfile establishes a builder that leverages the latest Golang version to build the dns-updater application. Upon completing the application build, Docker generates a container image. 
+
+For further information on creating the API key and secret, please visit https://developer.godaddy.com/getstarted.
+
+# Usage
+* Create a .env file and add the following environment variables
+```
+DDNS=<replace with apikey>
+DDNS_SEC=<replace with api secret>
+TZ=<your timezone>
+DDNS_INTERVAL=<replace with desired value e.g. 30m>
+DDNS_FILE=<location and file name of the dns records>
+```
+* To create the api key and secret goto https://developer.godaddy.com/getstarted
+
+* To build the docker image run the following command
+```
+docker build -t dns-updater:latest .
+```
+
+* After the build of the container image you're ready to create the dns-updater container
+```
+ docker run --name ddns --hostname ddns -dit --env-file ./.env dns-updater
+```
+
+# JSON
+Create a file called domains.json with the below structute in the sam folder as you main.go and Dockerfile. The most important fields are
+* domain: the domain needs to exist and owned by the api key and secret
+* name: this will be the name of the record
+* type: this is the type of record to add/update e.g. A, CNAME etc.
+* data: this is the value of the record e.g. you want to update a CNAME record 'home' for domain another_example.com to point to ip address 1.1.1.1 and the ttl must be 600 seconds
+```
+[
+  {
+    "domain": "example.com",
+    "type": "A",
+    "name": "@",
+    "ttl": 600,
+    "data": "",
+    "port": null,
+    "service": "",
+    "protocol": "",
+    "priority": null
+  },
+  {
+    "domain": "example.com",
+    "type": "CNAME",
+    "data": "@",
+    "name": "dns-updater",
+    "ttl": 600
+  },
+  {
+    "domain": "another_example.com",
+    "type": "CNAME",
+    "data": "1.1.1.1",
+    "name": "home",
+    "ttl": 600
+  }
+]
+
+```
+
+
---
+++ src/dns-updater/godaddy/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	apiKey    string
+	apiSecret string
+)
+
+type DnsRecord struct {
+	Domain   string `json:"domain,omitempty"`
+	Type     string `json:"type,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Data     string `json:"data"`
+	TTL      int    `json:"ttl,omitempty"`
+	Port     int    `json:"port,omitempty"`
+	Service  string `json:"service,omitempty"`
+	Protocol string `json:"protocol,omitempty"`
+	Priority int    `json:"priority,omitempty"`
+}
+
+func (r *DnsRecord) ToString() (string, error) {
+	recordBytes, err := json.Marshal(r)
+
+	if err != nil {
+		log.Println("Marshal error")
+		log.Printf("[{\"error\":\"%s\"}]", err.Error())
+		return fmt.Sprintf("[{\"error\":\"%s\"}]", err.Error()), err
+	}
+	return string(recordBytes), nil
+}
+
+func main() {
+	var updateInterval time.Duration
+	var recordsFile string
+
+	flag.DurationVar(&updateInterval, "interval", time.Minute*5, "Update interval (e.g., 5m)")
+	flag.StringVar(&recordsFile, "file", "domains.json", "File with all the DNS records defined(e.g., dnsrecords.json)")
+	flag.StringVar(&apiKey, "apiKey", "", "Godaddy api key e.g. -apiKey xyz")
+	flag.StringVar(&apiSecret, "apiSecret", "", "Godaddy api secret e.g apiSecret abc")
+	flag.Parse()
+
+	if apiKey == "" {
+		apiKey = os.Getenv("DDNS_KEY")
+	}
+	if apiSecret == "" {
+		apiSecret = os.Getenv("DDNS_SEC")
+	}
+
+	if apiKey == "" || apiSecret == "" {
+		log.Panicln("No api key or secret found")
+	}
+
+	for {
+		startTime := time.Now()
+		records, err := readRecordsFromFile(recordsFile)
+		if err != nil {
+			log.Fatalf("Error reading records from file: %v", err)
+		}
+
+		ip, err := getPublicIP()
+		if err != nil {
+			log.Printf("Error getting public IP: %v", err)
+		} else {
+			log.Printf("Current public IP: %s", ip)
+
+			log.Printf("records: %v", records)
+			for _, r := range records {
+				if r.Data == "" {
+					r.Data = ip
+				}
+				log.Printf("Domain: %s, %s", r.Domain, r.Data)
+
+				err := updateDNSRecord(r)
+				if err != nil {
+					log.Printf("Error updating DNS record: %v", err)
+				} else {
+					log.Printf("DNS record updated successfully")
+				}
+			}
+		}
+		ts := time.Since(startTime)
+		duration := fmt.Stringer.String(ts)
+		durationSec := float64(ts / time.Second)
+
+		log.Printf("[%s] Waiting %v, execution time: %s(%vs), next: %s, \n", time.Now(), updateInterval, duration, durationSec, time.Now().Add(updateInterval))
+		time.Sleep(updateInterval)
+	}
+}
+
+func getPublicIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func updateDNSRecord(record DnsRecord) error {
+	client := &http.Client{}
+
+	s, _ := record.ToString()
+	log.Printf("raw dns record: %s", s)
+
+	_domain := record.Domain
+	_type := record.Type
+	_name := record.Name
+
+	record.Domain = ""
+	record.Type = ""
+	record.Name = ""
+
+	recordBytes, err := json.Marshal([]DnsRecord{record})
+	if err != nil {
+		log.Println("Marshal error")
+		return err
+	}
+
+	log.Printf("marshaled dns record: %v", string(recordBytes))
+	log.Printf("https://api.godaddy.com/v1/domains/%s/records/%s/%s", _domain, _type, _name)
+	req, err := http.NewRequest(
+		"PUT",
+		fmt.Sprintf("https://api.godaddy.com/v1/domains/%s/records/%s/%s", _domain, _type, _name),
+		strings.NewReader(string(recordBytes)),
+	)
+	if err != nil {
+		log.Println("NewRequest Error")
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("sso-key %s:%s", apiKey, apiSecret))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Execute error (client.do)")
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("update failed: %d %s", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
+
+func readRecordsFromFile(filename string) ([]DnsRecord, error) {
+	fileBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	//log.Printf("file dns record: %s", string(fileBytes))
+	var records []DnsRecord
+	err = json.Unmarshal(fileBytes, &records)
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
