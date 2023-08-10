43a48bca3694326e4136614d5f44a1742581483f
albievan <albievan@gmail.com>
2023-08-10 10:36:43 +0100

    Added additional check

--- src/mousewiggler/main.go
+++ src/mousewiggler/main.go
@@ -9,18 +9,28 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
+var (
+	current_x int
+	current_y int
+)
+
 func doTask(sec int) {
 
 	for {
 		x, y := robotgo.GetMousePos()
 		high := 1.0
 		mouseDelay := 0.05
-		for i := 0; i < 5; i++ {
-			x1 := x + rand.Intn(100)
-			time.Sleep(10 * time.Millisecond)
-			robotgo.MoveSmooth(x1, y, high, mouseDelay)
-			time.Sleep(10 * time.Millisecond)
-			robotgo.MoveSmooth(x, y, high, mouseDelay)
+		if x == current_x || y == current_y {
+			for i := 0; i < 5; i++ {
+				x1 := x + rand.Intn(100)
+				time.Sleep(10 * time.Millisecond)
+				robotgo.MoveSmooth(x1, y, high, mouseDelay)
+				time.Sleep(10 * time.Millisecond)
+				robotgo.MoveSmooth(x, y, high, mouseDelay)
+			}
+			current_x, current_y = x, y
+		} else {
+			current_x, current_y = robotgo.GetMousePos()
 		}
 		time.Sleep(time.Duration(sec) * time.Second)
 
@@ -29,6 +39,7 @@ func doTask(sec int) {
 }
 
 func main() {
+	current_x, current_y = robotgo.GetMousePos()
 	secPtr := flag.Int("sec", 300, "-sec xxx")
 	flag.Parse()
 	doTask(*secPtr)
